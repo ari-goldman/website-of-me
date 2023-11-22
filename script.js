ec8d3359fb0543eaca007572b247a3eb252b03f6
ari-goldman <112101394+ari-goldman@users.noreply.github.com>
2023-11-22 02:03:53 -0700

    made it 1 track thingy, and colors and coolness and nobody is gonna ready this

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const alphabet = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9 ! @ # $ % ^ & * ( ) { } [ ] < > - + _ = : ; \" ' , . ? / \\"
 const alphabetSpaceless = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*(){}[]<>-+_=:;\"',.?/\\"
-const tabs = ['aaaaaa','about me', 'my stuff', 'links','other']
-const charsPerSelector = 9
+const tabs = ['about_me','portfolio','links','skills']
+const tabColorsSelected = ['#ea323c','#5ac54f','#00cdf9','#ca52c9']
+const tabColorsUnelected = ['#571c27','#134c4c','#00396d','#3b1443']
+var tabSquish = ""
+var tabIndices = []
+tabs.forEach((tab) => {
+    tabIndices.push(tabSquish.length)
+    tabSquish += tab
+})
+
+console.log(tabs, tabSquish, tabIndices)
+const charsPerSelector = tabSquish.length
 
 const fillScrollSelector = (chars) => {
     const selectors = document.querySelectorAll(".scroll-selector")
-    console.log(selectors)
     selectors.forEach((selector) => {
         for(var i = 0; i < chars; i++){
             var selectorContainer = document.createElement("div")
             selectorContainer.classList.add("scroll-selector-container")
+            // selectorContainer.createAttribute('data-tab')
             var selectorItem = document.createElement("span")
             selectorItem.classList.add("scroll-selector-item")
             selectorItem.innerHTML = alphabet
@@ -22,11 +32,6 @@ const fillScrollSelector = (chars) => {
 fillScrollSelector(charsPerSelector)
 var selectors = document.querySelectorAll(".scroll-selector")
 
-var changeSelection = true
-var currentSelection = 0
-let totalScrollDistance = 500
-let scrollTimeout;
-const scrollTolerance = 1000;
 var timeAtLastChange = 0
 document.addEventListener("wheel", function(e) {
     let d = new Date()
@@ -44,36 +49,84 @@ document.addEventListener("wheel", function(e) {
 
    
     selectors.forEach((selector) => {
-        let t = parseInt(selector.getAttribute('data-tab'))
+        let t = parseInt(selector.getAttribute('data-tab-selected'))
         t += incdec
         t %= tabs.length
         if(t < 0) t = tabs.length - 1
-        console.log(selectors,t)
-        scrollToWord(selector,tabs[t])
-        selector.setAttribute('data-tab',`${t}`)
+        // console.log(selectors,t)
+        // scrollToWord(selector,tabs[t])
+        setTab(selector, t)
     })
     // console.log(currentSelection)
     // scrollTimeout = setTimeout(() => { return }, 2000); // Adjust the timeout duration as needed
 })
 
-var currentWord = "ARGHH"
+const scrollToRepeatingString = (selector, string) => {
+    string = ("" + string).toUpperCase()
+    selectorItems = selector.querySelectorAll('.scroll-selector-container .scroll-selector-item')
+    for(let i = 0; i < charsPerSelector; i++){
+        scrollToLetter(selectorItems[i], string[i % string.length])
+    }
+}
+
 const scrollToWord = (selector, word) => {
     word = ("" + word).toUpperCase()
     let a = 'A'.charCodeAt(0)
     selectorItems = selector.querySelectorAll('.scroll-selector-container .scroll-selector-item')
     for(let i = 0; i < charsPerSelector; i++){
-        // distance = Math.abs(word.charCodeAt(i) - currentWord.charCodeAt(i))
-        // selectors[i].style.transition = `transform ${distance * 100}ms ease-out`
-        if(word.length > i && alphabetSpaceless.replace(' ', '').indexOf(word[i]) > -1)
-            selectorItems[i].style.transform = `translateY(${-0.2 -1.5 * Math.trunc(alphabet.indexOf(word[i]) / 2)}em)`
+        if(word.length > i)
+            scrollToLetter(selectorItems[i], word[i])
         else
             selectorItems[i].style.transform = `translateY(${1.5}em)`
     }
     currentWord = word
 }
 
+const scrollToLetter = (selectorItem, char) => {
+    if(alphabetSpaceless.indexOf(char) > -1)
+        selectorItem.style.transform = `translateY(${-0.2 -1.5 * Math.trunc(alphabet.indexOf(char) / 2)}em)`
+    else
+        selectorItem.style.transform = `translateY(${1.5}em)`
+}
+
+
+const colorSelector = (selector, begin, end) => {
+    selectorContainers = selector.querySelectorAll('.scroll-selector-container')
+    for(var i = 0; i < selectorItems.length; i++){
+        if(i >= begin && i < end){
+            // console.warn(tabColorsSelected[parseInt(selectorItems[i].getAttribute('data-tab'))])
+            selectorContainers[i].style.color = tabColorsSelected[parseInt(selectorContainers[i].getAttribute('data-tab'))]
+        }else{
+            selectorContainers[i].style.color = tabColorsUnelected[parseInt(selectorContainers[i].getAttribute('data-tab'))]
+        }
+    }
+}
+
+
+const assignTabs = (selector, initial) => {
+    var curTab = 0
+    selectorContainers = selector.querySelectorAll('.scroll-selector-container')
+    var i = 0
+    for(var t = 0; t < tabs.length; t++){
+        curT = (t+initial) % tabs.length
+        for(var j = 0; j < tabs[curT].length; j++){
+            selectorContainers[i].setAttribute('data-tab',`${curT}`)
+            selectorContainers[i].onclick = function() {
+                setTab(selector, parseInt(this.getAttribute('data-tab')))
+            }
+            i++;
+        }
+    }
+}
+
+const setTab = (selector, t) => {
+    console.log("Tab set to " + t)
+    scrollToRepeatingString(selector,tabSquish.substring(tabIndices[t], tabSquish.length) + tabSquish.substring(0, tabIndices[t]))
+    assignTabs(selector, t)
+    colorSelector(selector, 0, tabs[t].length)
+    selector.setAttribute('data-tab-selected',`${t}`)
+}
+
 selectors.forEach((selector) => {
-    scrollToWord(selector, tabs[parseInt(selector.getAttribute('data-tab'))])
+    setTab(selector, 0)
 })
-
-
